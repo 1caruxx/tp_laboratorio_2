bc8db08579b0377b3adc222daa84a9b1bf3f1e4e
1caruxx <fernandohugolareu@hotmail.com>
2017-06-20 15:05:55 -0300

    Trabajo practico 4
    Se ha aniadido el trabajo practico numero 4.

---
+++ Navegador TP-4 - AlumnoV2/Archivos/Texto.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        private string _archivo;
+
+        public Texto(string archivo)
+        {
+            this._archivo = archivo;
+        }
+
+        public bool guardar(string datos)
+        {
+            try
+            {
+                using (StreamWriter escritor = File.AppendText(this._archivo))
+                {
+                    escritor.WriteLine(datos);
+                }
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return true;
+        }
+
+        public bool leer(out List<string> datos)
+        {
+            List<string> listaAuxiliar = new List<string>();
+            string linea;
+
+            try
+            {
+                using (StreamReader lector = new StreamReader(this._archivo))
+                {
+                    while ((linea = lector.ReadLine()) != null)
+                    {
+                        listaAuxiliar.Add(linea);
+                    }
+                }
+
+                datos = listaAuxiliar;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return true;
+        }
+    }
+}
---
+++ Navegador TP-4 - AlumnoV2/Hilo/Descargador.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * Aclaracion: Eventos y threads no han sido dados en clase aun por lo que a pesar de que el programa funcione correctamente, no esta hecho de la forma en la que se esperaba.
+ * Gran parte de lo logrado fue gracias a investigacion a traves de internet. Agradeceria mucho tener en cuenta este facotr.
+ * Gracias por su atencion.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Net; // Avisar del espacio de nombre
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace Hilo
+{
+    public class Descargador
+    {
+        private string html;
+        private Uri direccion;
+
+        public Descargador(Uri direccion)
+        {
+            this.html = direccion.ToString();
+            this.direccion = direccion;
+        }
+
+        public void IniciarDescarga()
+        {
+            try
+            {
+                WebClient cliente = new WebClient();
+                cliente.DownloadProgressChanged += new DownloadProgressChangedEventHandler(WebClientDownloadProgressChanged);
+                cliente.DownloadStringCompleted += new DownloadStringCompletedEventHandler(WebClientDownloadCompleted);
+
+                cliente.DownloadStringAsync(this.direccion);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+
+        private void WebClientDownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e)
+        {
+            ((ToolStripProgressBar)sender).Increment(e.ProgressPercentage);
+        }
+        private void WebClientDownloadCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (!e.Cancelled && e.Error == null)
+            {
+                string textString = (string)e.Result;
+
+                MessageBox.Show(textString);
+            }
+        }
+    }
+}
